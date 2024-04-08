cb085d2fff1936c6a1a934788134488e8e863c98
AbdulRahman Badran <ambadran717@hotmail.com>
2024-04-08 14:50:11 +0200

    added axis_control circuit and programming files

---
+++ Programming/axis_controller/README.md
@@ -0,0 +1,3 @@
+# STC8G1K08 Axis MCU
+
+Take in commands from Main MCU through nRF24l01 and gives instructions to two stepper motor drivers DRV8825 to move the railing mechanism through aluminum profiles
---
+++ Programming/axis_controller/main.c
@@ -0,0 +1,47 @@
+#include "project-defs.h"
+
+void main(void) {
+
+  // inits
+  INIT_EXTENDED_SFR();
+  EA = 1; // enable interrupts
+  serialConsoleInitialise(
+      CONSOLE_UART, 
+      CONSOLE_SPEED, 
+      CONSOLE_PIN_CONFIG
+      );
+  gpio_init();
+
+  // Awaiting User picocom
+  // TODO: add a debug switch that switches on this code for debug mode or ignore this code for normal mode
+  uint8_t initial_char = '0';
+  while (initial_char != 's') {
+    initial_char = uartGetCharacter(CONSOLE_UART, NON_BLOCKING);
+  }
+  uartSendBlock(CONSOLE_UART, "Starting..\n", 12, NON_BLOCKING);
+
+  //initializing nrf24l01+ as a receiver device with one simpl function call
+  nrf24_device(RECEIVER, RESET);
+
+  // Main Routine
+  char received_payload;
+  char led_state;
+  while (1) {
+
+    if (nrf24_receive(&received_payload, 1) != RECEIVE_FIFO_EMPTY) {  
+      printf("\rReceived Value: %c\n", received_payload);
+      led_state = received_payload;
+    }
+
+    if (led_state == '1') {
+      test_pin_on();
+    } else if (led_state == '0') {
+      test_pin_off();
+    }
+
+  }
+
+
+
+
+}
